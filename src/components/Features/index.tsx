74842fb8972494b0296f3cceb1cacefd18053881
nitinkumar26 <kumarnitin30121@gmail.com>
2025-03-31 19:01:39 +0530

    feat-sec-up

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -13,11 +13,11 @@ const Features = () => {
             center
           />
 
-          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
-            {featuresData.map((feature) => (
-              <SingleFeature key={feature.id} feature={feature} />
-            ))}
-          </div>
+<div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      {featuresData.map((feature) => (
+        <SingleFeature key={feature.id} feature={feature} />
+      ))}
+    </div>
         </div>
       </section>
     </>
