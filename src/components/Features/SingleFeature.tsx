e9df240efe82ddba4d08fd77900192239216aac8
nitinkumar2026 <kumarnitin30121@gmail.com>
2025-05-06 13:04:02 +0530

    new blog and footer update

--- src/components/Features/SingleFeature.tsx
+++ src/components/Features/SingleFeature.tsx
@@ -1,20 +1,11 @@
 "use client";
 
 import { Feature } from "@/types/feature";
-import { motion } from "framer-motion";
-
 const SingleFeature = ({ feature }: { feature: Feature }) => {
   const { icon, title, paragraph } = feature;
 
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 30 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true }}
-      transition={{ duration: 0.2, ease: "easeOut" }}
-      whileHover={{ scale: 1.02 }}
-      className="w-full bg-white rounded-3xl p-6 transition-all duration-300"
-    >
+    <div className="w-full bg-white rounded-3xl p-6 transition-all duration-300">
       <div className="flex justify-between items-start gap-4 py-4">
         {/* Title Section */}
         <div className="flex-1">
@@ -27,20 +18,16 @@ const SingleFeature = ({ feature }: { feature: Feature }) => {
         </div>
 
         {/* Icon Section */}
-        <motion.div
-          whileHover={{ rotate: 8 }}
-          transition={{ type: "spring", stiffness: 300 }}
-          className="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-[#F2F5FF] text-[#a31d1d]"
-        >
+        <div className="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-[#F2F5FF] text-[#a31d1d]">
           {icon}
-        </motion.div>
+        </div>
       </div>
 
       {/* Paragraph */}
       <p className="pt-6 pb-3 pr-[10px] sm:text-sm md:text-md lg:text-base text-black font-normal leading-relaxed font-['Poppins']">
         {paragraph}
       </p>
-    </motion.div>
+    </div>
   );
 };
 
